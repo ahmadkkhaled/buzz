5ca0597d5bb3ba230fd562344199ffbcf098e158
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-03-08 23:21:44 +0200

    Added path 1 gizmo and ping pong path iteration

---
+++ Assets/Scripts/PathDefinition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PathDefinition : MonoBehaviour
+{
+    public Transform[] Points;
+    public IEnumerator<Transform> GetPathEnumerator() /// IEnumerator is similar to an iterator.
+    {
+        if (Points == null || Points.Length < 1)
+            yield break;
+
+        var direction = 1;
+        var index = 0;
+        while (true)
+        {
+            yield return Points[index]; // yield control back to caller and return Points[index];
+
+            if (Points.Length == 1)
+                continue;
+
+            if (index <= 0)
+                direction = 1;
+            else if (index >= Points.Length - 1)
+                direction = -1;
+            index += direction;
+        }
+    }
+    public void OnDrawGizmos()
+    {
+        if (Points == null) 
+            return;
+
+        for(int i = 1; i<Points.Length; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+        }
+    }
+}
