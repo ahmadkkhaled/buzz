89da74c6bb2f91d61aaa0d49d23eab5e7176f8e5
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-05-06 12:49:11 +0200

    005-horizontal-movement 00:31:43

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -13,27 +13,48 @@ public class CharacterController2D : MonoBehaviour
     public LayerMask PlatformMask;
     public ControllerParameters2D DefaultParameters;
     public ControllerState2D State { get; private set; }
-    public Vector2 Velocity { get; private set; }
+    public Vector2 Velocity { get { return _velocity; } } // structs are returned by value
     public bool CanJump { get { return false; } }
+    public bool HandleCollisions { get; set; }
+    public ControllerParameters2D Parameters { get { return _overrideParameters ?? DefaultParameters; } }
+
+    private Vector2 _velocity;
+    private Transform _transform;
+    private Vector3 _localScale;
+    private BoxCollider2D _boxCollider;
+    private ControllerParameters2D _overrideParameters;
+
+    private float
+        _verticalDistanceBetweenRays,
+        _horizontalDistanceBetweenRays;
     public void Awake()
     {
         State = new ControllerState2D();
+        _transform = transform; // aliasing to prevent built-in chain calls
+        _localScale = transform.localScale;
+        _boxCollider = GetComponent<BoxCollider2D>();
+
+        var colliderWidth = _boxCollider.size.x * Mathf.Abs(transform.localScale.x) - 2 * SkinWidth;
+        _horizontalDistanceBetweenRays = colliderWidth / (TotalVerticalRays - 1);
+
+        var colliderHeight = _boxCollider.size.y * Mathf.Abs(transform.localScale.y) - 2 * SkinWidth;
+        _verticalDistanceBetweenRays = colliderHeight / (TotalHorizontalRays - 1);
     }
     public void AddForce(Vector2 force)
     {
-
+        _velocity += force;
     }
     public void SetForce(Vector2 force)
     {
-
+        _velocity = force;
     }
     public void SetHorizontalForce(float x)
     {
-
+        _velocity.x = x;
     }
     public void SetVerticalForce(float y)
     {
-
+        _velocity.y = y;
     }
 
     public void Jump()
@@ -43,12 +64,39 @@ public class CharacterController2D : MonoBehaviour
 
     public void LateUpdate()
     {
-        
+        Move(Velocity * Time.deltaTime);
     }
 
     private void Move(Vector2 deltaMovement)
     {
+        var wasGrounded = State.IsCollidingBelow;
+        State.Reset();
 
+        if (HandleCollisions)
+        {
+            HandlePlatforms();
+            CalculateRayOrigins();
+
+            if (deltaMovement.y < 0 && wasGrounded)
+                HandleVerticalSlope(ref deltaMovement);
+
+            if (Mathf.Abs(deltaMovement.x) > .001f)
+                MoveHorizontally(ref deltaMovement);
+
+            MoveVertically(ref deltaMovement);
+        }
+
+        _transform.Translate(deltaMovement, Space.World);
+
+
+        if (Time.deltaTime > 0)
+            _velocity = deltaMovement / Time.deltaTime;
+
+        _velocity.x = Mathf.Min(_velocity.x, Parameters.MaxVelocity.x);
+        _velocity.y = Mathf.Min(_velocity.y, Parameters.MaxVelocity.y);
+
+        if (State.IsMovingUpSlope)
+            _velocity.y = 0;
     }
 
     private void HandlePlatforms()
